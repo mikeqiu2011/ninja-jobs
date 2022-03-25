827b9735a796b71c7ae8a147bf90b93f75208f89
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-25 19:22:55 +0800

    first commit

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Job Details</h1>
+  <!-- <p>The job id is {{ $route.params.id }}</p> -->
+  <p>The job id is {{ id }}</p>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
